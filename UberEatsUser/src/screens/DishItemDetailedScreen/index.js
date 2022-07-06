fdde976161596d29bf3c1b6917ad4bf005f8bd7f
kuda214 <kudachivunga33@gmail.com>
2022-07-06 23:16:44 +0200

    💄 (UI)Add dish to basket page

--- UberEatsUser/src/screens/DishItemDetailedScreen/index.js
+++ UberEatsUser/src/screens/DishItemDetailedScreen/index.js
@@ -1,15 +1,56 @@
 import { View,Text } from "react-native";
 import styles from "./style";
 import resturants from '../../../assets/data/restaurants.json'
+import {AntDesign} from '@expo/vector-icons';
+import { useState } from "react";
 
-const dish = resturants[0].dishes[0];
+
+const dish = resturants[2].dishes[0];
+// const quantity = 0; wont work we need to rerender
 
 const DishDetailedScreen = () => {
+
+    const [quantity,setQuantity] = useState(1); //array of 2 values
+    const [totalPrice, setTotalPrice] = useState(dish.price);
+    //vale = 2 wont work so setter(2)
+
+    const onMinus = () =>{
+
+        if(quantity > 1)
+        {
+            setQuantity(quantity -1);
+            setTotalPrice(totalPrice - dish.price);
+
+        }
+   
+    }
+    const onPlus = () =>
+    {
+        setQuantity(quantity + 1);
+        setTotalPrice(totalPrice + dish.price);
+
+    }
+
     return (
         <View style={styles.page}>
-            <Text style={styles.name}>{dish.name}</Text>
-            <Text style={styles.description}>{dish.description}</Text>
-            <View style={styles.horizontalSEperator}></View>
+            <View style={styles.dishInfo}>
+                <Text style={styles.name}>{dish.name}</Text>
+                <Text style={styles.description}>{dish.description}</Text>
+                <View style={styles.horizontalSeperator}></View>
+            </View>
+
+            <View style={styles.row}>
+                <AntDesign name="minuscircleo" size={45} color={"black"} onPress={onMinus}/>
+                <Text style={styles.quantity}>{quantity}</Text>
+                <AntDesign name="pluscircleo" size={45} color={"black"} onPress={onPlus}/>
+
+            </View>
+
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>
+                    Add {quantity} to basket &nbsp;&nbsp; R{totalPrice}
+                </Text>
+            </View>
         </View>
     );
 }
--- UberEatsUser/src/screens/DishItemDetailedScreen/style.js
+++ UberEatsUser/src/screens/DishItemDetailedScreen/style.js
@@ -4,9 +4,54 @@ const styles = StyleSheet.create({
     page:{
         flex: 1,
         backgroundColor: '#fff',
-        justifyContent: 'center',
+        width:'100%',
+    },
+    name:{
+        
+        fontSize:30,
+        fontWeight:'600',
+        marginVertical:10,
+    
+    },
+    description:{
+        color:"#696969",
+        fontSize:16,
+    },
+    dishInfo:{
+        margin:30,
+    },
+    horizontalSeperator:{
+        height:1,
+        backgroundColor:'lightgrey',
+        marginVertical:12,
+    },
+    row:{
+        flexDirection:'row',
         alignItems:'center',
+        justifyContent:'center',
+        marginTop:10,
+    },
+    quantity:{
+        fontSize:30,
+        marginHorizontal:20,
+        fontWeight:'500',
+    },
+    button:
+    {
+        backgroundColor:"#000",
+        bottom:10,
+        position:'absolute',
+        width:'100%',
+        alignItems:'center',
+        color:'#fff',
+        padding:20,
+    },
+    buttonText:{
+        fontSize:20,
+        fontWeight:'600',
+        color:"#fff",
     }
+
 });
 
 export default styles;
