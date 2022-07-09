9de2f34db9bd7071557c2ab99a467a0dca7f2d97
kuda214 <kudachivunga33@gmail.com>
2022-07-09 13:33:20 +0200

    💄 (UI) Added Profile Screen

---
+++ UberEatsUser/src/screens/ProfileScreen/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 28,
+        fontWeight: "bold",
+        textAlign: "center",
+        margin: 10,
+      },
+      input: {
+        margin: 10,
+        backgroundColor: "white",
+        padding: 15,
+        borderRadius: 5,
+      },
+    saveButton:{
+        backgroundColor: '#000',
+        
+    }
+});
+
+export default styles;
