907fa75baa47f034c2acc6ccb1b08c7e5fc4727b
kuda214 <kudachivunga33@gmail.com>
2022-07-17 22:55:58 +0200

    Added antd

---
+++ uber-eats-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Card, Descriptions, Divider, List, Button} from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+     <span>Hey</span>
+    </div>
+  );
+}
+
+export default App;
